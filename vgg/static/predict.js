42b5e44d4c5568d0b44a4ec127822b3f52a2007d
dhiraj <baruahd5@gmail.com>
2020-09-14 18:44:17 +0530

    pre-processing added

--- vgg/static/predict.js
+++ vgg/static/predict.js
@@ -21,11 +21,37 @@ $('#predict-button').click(async function(){
     let tensor =tf.browser.fromPixels(image)
     .resizeNearestNeighbor([224,224])
     .toFloat()
+ 
+    let meanImageNetRGB ={
+        red:123.68,
+        green:116.779,
+        blue:103.939
+    };
+
+    let indices = [
+        tf.tensor1d([0], "int32"),
+        tf.tensor1d([1], "int32"),
+        tf.tensor1d([2], "int32")
+    ];
+
+    let centeredRGB={
+        red: tf.gather(tensor, indices[0],2)
+        .sub(tf.scalar(meanImageNetRGB.red))
+        .reshape([50176]),
+        green: tf.gather(tensor, indices[1],2)
+        .sub(tf.scalar(meanImageNetRGB.green))
+        .reshape([50176]),
+        blue: tf.gather(tensor, indices[2],2)
+        .sub(tf.scalar(meanImageNetRGB.blue))
+        .reshape([50176])
+    };
+
+    let processedTensor = tf.stack([centeredRGB.red, centeredRGB.green, centeredRGB.blue],1)
+    .reshape([224,224,3])
+    .reverse(2)
     .expandDims();
 
-    //More pre-processing to be added here later
-
-    let prediction = await model.predict(tensor).data();
+    let prediction = await model.predict(processedTensor).data();
     let top5= Array.from(prediction)
     .map(function(p,i){
         return {
